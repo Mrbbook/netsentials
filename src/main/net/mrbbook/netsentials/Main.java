4178376b66ca4626acbb0282ad39d9bf18b3f116
Tanawat Boonmak <latte.book@gmail.com>
2016-05-08 22:14:29 +0700

    Create Main.java

---
+++ src/main/net/mrbbook/netsentials/Main.java
@@ -0,0 +1,26 @@
+package net.mrbbook.netsentials;
+
+
+
+//ขออนุญาติเว้นไวทำการ include class มาเพิ่มหลังจากลง eclipse แล้วนะครับ
+
+
+public class Main extends Javaplugin {
+
+
+public void onEnable(){
+
+}
+
+
+
+
+public void 
+
+
+
+
+
+
+
+}
